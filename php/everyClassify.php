c2b4bcb84afe04740f4548810e16d9e4f7ec1b04
zhengxn <zxn3069@outlook.com>
2017-08-13 11:10:08 +0800

    Delete everyClassify.php

--- php/everyClassify.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    header("Content-Type:text/html;charset=utf-8");
-    $start=isset($_GET['start']) ? $_GET['start']:0;
-    $cid=isset($_GET['cid']) ? $_GET['cid']:36;
-	$str3=file_get_contents("http://wallpaper.apc.360.cn/index.php?c=WallPaper&a=getAppsByCategory&cid={$cid}&start={$start}&count=9&from=360chrome");
-	echo $str3;
-?>
